039ff5da087b65afdf3c75926a572366e2f393b2
MutatedGamer <lukebordonaro@gmail.com>
2020-04-26 13:21:53 -0400

    fix typo in settings

--- resume_django3/settings.py
+++ resume_django3/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
