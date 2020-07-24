b480676a5a44e6a1d34a1257d148f427612b4a0e
MutatedGamer <lukebordonaro@gmail.com>
2020-07-23 19:19:13 -0700

    Use SSL mode on DB

--- resume_django3/settings.py
+++ resume_django3/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,6 +91,7 @@ if os.environ['ENV'] == 'production':
             'HOST': os.environ['DBHOST'],
             'USER': os.environ['DBUSER'],
             'PASSWORD': os.environ['DBPASS']
+            'sslmode': 'require'
         }
     }
 
@@ -192,4 +192,3 @@ LOGGING = {
     }
 }
 
-django_heroku.settings(locals(), staticfiles=False)
