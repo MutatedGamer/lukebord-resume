b4fd0c1f5f1262cc5bbec7e928dbf95cee0c4e9f
MutatedGamer <lukebordonaro@gmail.com>
2020-07-25 15:33:49 -0700

    allow www.

--- resume_django3/settings.py
+++ resume_django3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'lukebord-resume.herokuapp.com', 'lukebord.com', 'lukebordonaro.com', 'lukebord-resume-2.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1',  'www.lukebord.com', 'www.lukebordonaro.com', 'lukebord.com', 'lukebordonaro.com', 'lukebord-resume-2.azurewebsites.net']
 
 
 # Application definition
